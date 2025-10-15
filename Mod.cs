2d835f602e1d7af6d4c44bd66cfba22578985178
Tosox <57193602+Tosox@users.noreply.github.com>
2025-10-15 17:11:43 +0200

    refactor: Extract ModMetadata

--- Mod.cs
+++ Mod.cs
@@ -1,28 +1,9 @@
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
-using SPTarkov.Server.Core.Models.Spt.Mod;
 using SPTarkov.Server.Core.Models.Utils;
-
-using Range = SemanticVersioning.Range;
-using Version = SemanticVersioning.Version;
 
 namespace Tosox.FIRFencePurchases
 {
-    public record ModMetadata : AbstractModMetadata
-    {
-        public override string ModGuid { get; init; } = "de.tosox.firfencepurchases";
-        public override string Name { get; init; } = "FIR Fence Purchases";
-        public override string Author { get; init; } = "Tosox";
-        public override List<string>? Contributors { get; init; }
-        public override Version Version { get; init; } = new("1.0.0");
-        public override Range SptVersion { get; init; } = new("~4.0.0");
-        public override List<string>? Incompatibilities { get; init; }
-        public override Dictionary<string, Range>? ModDependencies { get; init; }
-        public override string? Url { get; init; } = "https://github.com/Tosox/SPT-FIRFencePurchases";
-        public override bool? IsBundleMod { get; init; }
-        public override string License { get; init; } = "MIT";
-    }
-
     [Injectable(TypePriority = OnLoadOrder.PreSptModLoader)]
     public class FIRFencePurchasesPre(
         ISptLogger<FIRFencePurchasesPre> logger
