0412e47b107684179f4fed0558de92564d321f49
Tosox <57193602+Tosox@users.noreply.github.com>
2025-10-15 16:57:43 +0200

    feat: SPT4 support

---
+++ Patches/FenceServicePatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using SPTarkov.Reflection.Patching;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Services;
+using System.Reflection;
+
+namespace Tosox.FIRFencePurchases.Patches
+{
+    public class FenceServicePatch : AbstractPatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(FenceService), nameof(FenceService.GetFenceAssorts));
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix(ref TraderAssort __result)
+        {
+            __result.Items = [.. __result.Items
+                .Select(item => {
+                    item.Upd ??= new Upd();
+                    item.Upd.SpawnedInSession = true;
+                    return item;
+                })];
+        }
+    }
+}
